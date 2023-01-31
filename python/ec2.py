c615ad510bfd0e87a204fe74e6f54581c34cb860
Flub78 <frederic.peignot@free.fr>
2023-01-31 17:49:19 +0100

    Refactoring ec2.py. The script now select a list of instances on which the action is applied.

--- python/ec2.py
+++ python/ec2.py
@@ -5,37 +5,15 @@
 AWS EC2 instances management.
 
 The scripts are able to list, create and delete EC2 instances.
-
-TODO:   support of delete with instance name
 """
 import argparse
 import os
-import boto3
-import json
+from lib.aws_ec2 import *
 
 # Initialization
 # --------------
 description="aws EC2 instances management"
 resource='ec2_instance'
-basename = os.environ.get('BASENAME')
-
-ec2 = boto3.resource('ec2')
-ec2_client = boto3.client('ec2')
-
-amazon_linux_2="ami-0cc814d99c59f3789"
-jenkins_7="ami-0e019d7e4645e931d"
-ubuntu="ami-0afd55c0c8a52973a"
-ami=ubuntu
-
-instancetype='t2.micro'
-sg='sg-0b864f5c5c2ceb714'
-
-keyname = 'ec2-keypair'
-instancename = 'ec2-i'
-if basename:
-    keyname = basename + '_' + keyname
-    instancename = basename + '_' + instancename
-instanceid = ""
 
 # Parsing of the CLI arguments
 # ----------------------------
@@ -49,166 +27,31 @@ group.add_argument('-s', '--stop', action='store_true', help='stop an ' + resour
 group.add_argument('-r', '--resume', action='store_true', help='restart an ' + resource)
 
 parser.add_argument('-v', '--verbose', action='store_true', help='verbose mode')
-# parser.add_argument('--int', type=int, help='an integer value')
-# parser.add_argument('--float', type=float, help='a float value')
-parser.add_argument('-i', '--instance', type=str, help='EC2 instance ID')
+
+parser.add_argument('-i', '--id', type=str, help='EC2 instance ID')
 parser.add_argument('-n', '--name', type=str, help='name of the ' + resource + ' to create')
 parser.add_argument('-f', '--filter', type=str, help='process only the matching strings')
 parser.add_argument('-k', '--keypair', type=str, help='keypair to use')
 parser.add_argument('-t', '--type', type=str, help='instance type')
 parser.add_argument('-a', '--ami', type=str, help='image')
-# parser.add_argument('--bool', type=bool, help='a boolean value')
 
 args = parser.parse_args()
 
 ###################################################
-def list():
-    """ list all the resources """
-    if args.verbose:
-        print ('list the ' + resource)
-    response = ec2_client.describe_instances()
-    # print(response)
-
-    # Iterate through each instance and print its ID
-    for reservation in response['Reservations']:
-        for instance in reservation['Instances']:
-            # json_string = json.dumps(instance, indent=2, default=str)
-            # print(json_string)
-
-            str = ''
-            if 'Tags' in instance:
-                for tag in instance['Tags']:
-                    if (tag['Key'] == 'Name'):
-                        str = tag['Value'] + ' '
-
-            str += instance['InstanceId'] + ' '
-            str += instance['ImageId'] + ' '
-            str += instance['InstanceType'] + ' '
-            str += instance['State']['Name'] + ' '
-            str += instance['Placement']['AvailabilityZone'] + ' '
-            str += instance['PublicDnsName'] + ' '
-            if (args.filter):
-                if args.filter in str:
-                    print(str)
-            else:
-                print(str)
-
-def name_to_id(needle, reverse=False):
-    """ look for the id matching a name """
- 
-    response = ec2_client.describe_instances()
-    # print(response)
-
-    # Iterate through each instance and print its ID
-    for reservation in response['Reservations']:
-        for instance in reservation['Instances']:
-            # json_string = json.dumps(instance, indent=2, default=str)
-            # print(json_string)
-
-            if 'Tags' in instance:
-                for tag in instance['Tags']:
-                    if (tag['Key'] == 'Name'):
-                        ec2_name = tag['Value']
-
-            ec2_id = instance['InstanceId']
-
-            if reverse and (ec2_id == needle):
-                return ec2_name
-            
-            if (ec2_name == needle):
-                return ec2_id
-             
-    return ""
-
-
-def create():
-    """ create a resource """
-   
-    if args.verbose:
-        print ('creating ' + resource)
-
-    # create a new EC2 instance
-    instances = ec2.create_instances(
-        ImageId=ami,
-        MinCount=1,
-        MaxCount=1,
-        InstanceType=instancetype,
-        KeyName=keyname,
-        SecurityGroupIds=[sg],
-        TagSpecifications=[{
-            'ResourceType': 'instance',
-            'Tags': [
-                {
-                    'Key': 'Name',
-                    'Value': instancename
-                },
-            ]
-        }]
-    )
-
-def delete():
-    """ delete a resource """
-    if not args.instance:
-        print ("instance ID is required")
-        exit
-
-    if args.verbose:
-        print ('deleting ' + resource, args.instance)
-
-    ec2.instances.filter(InstanceIds = [args.instance]).terminate()
- 
-
-def resume(id):
-    """ resume an ec2 instance """
-
-    if not id:
-        print ("instance ID is required")
-        exit
-
-    if args.verbose:
-        print ('resuming ' + resource, id)
-
-    ec2_client.start_instances(InstanceIds=[id])
-    
-def stop(id):
-    """ stop an ec2 instance """
-
-    if not id:
-        print ("instance ID is required")
-        exit
-
-    if args.verbose:
-        print ('stopping ' + resource, id)
-
-    ec2_client.stop_instances(InstanceIds=[id])
-###################################################
 # Main processing
-
-if args.name:
-    instancename = args.name
-    instanceid = name_to_id(instancename)
-    # print ('name = ' + instancename + ', id = ' + instanceid)
-
-if args.instance:
-    instanceid = args.instance
-    instancename = name_to_id(instanceid, True)
-    # print ('name = ' + instancename + ', id = ' + instanceid)
+ids = ec2_select_ids(verbose=args.verbose, name=args.name, id=args.id, filter=args.filter)
 
 if args.list:
-    list()    
+     ec2_print_list(ids, args.verbose)
 
 if args.create:
-    create()
+    ec2_create(name=args.name, keyname=args.keypair, verbose=args.verbose)
 
 if args.delete:
-    delete()
+    ec2_delete(ids, args.verbose)
 
 if args.resume:
-    resume(instanceid)
-    args.filter = instanceid
-    list()
+    ec2_resume(ids, args.verbose)
 
 if args.stop:
-    stop(instanceid)
-    args.filter = instanceid
-    list()
+    ec2_stop(ids, args.verbose)
--- python/lib/aws_ec2.py
+++ python/lib/aws_ec2.py
@@ -15,79 +15,128 @@ import os
 ec2_client = boto3.client('ec2')
 ec2 = boto3.resource('ec2')
 
-def ec2_list(verbose = False, filter = ""):
-    """ return a list of EC2 instances"""
-    response = ec2_client.describe_key_pairs()
-    if (verbose):
-        print("Key pair list")
-        print_json(response)
-
-    if (filter):
-        list = []
-        for key in response['KeyPairs']:
-            str = key['KeyName'] + key['KeyPairId'] +  key['KeyType']
-            if filter in str:
-                list.append(key)
-        return list
-    
-    else:
-        return response['KeyPairs']
-
-
 def ec2_select_ids(verbose = False, name="", id="", filter=""):
     """ 
-        return a list of ids
+        return a list of instances
     """
-    response = ec2_client.describe_key_pairs()
- 
+    response = ec2_client.describe_instances()
+
+    if verbose:
+        print("EC2 instances")
     list = []   
+    ids = []
+    for reservation in response['Reservations']:
+        for instance in reservation['Instances']:
 
-    for key in response['KeyPairs']:
-        str = key['KeyName'] + key['KeyPairId'] +  key['KeyType']
+            str = ''
+            if 'Tags' in instance:
+                for tag in instance['Tags']:
+                    if (tag['Key'] == 'Name'):
+                        instancename = tag['Value']
 
-        if (name and (name == key['KeyName'])):
-            if (not key in list):
-                if (not filter or (filter in str)):
-                    list.append(key)
-        elif (id and (id == key['KeyPairId'])):
-            if (not key in list):
-                if (not filter or (filter in str)):
-                    list.append(key)
-        else:
-            if (filter):
-                if ((filter in str) and (not key in list)):
-                    list.append(key)
+            str = instancename + ' '
+            str += instance['InstanceId'] + ' '
+            str += instance['ImageId'] + ' '
+            str += instance['InstanceType'] + ' '
+            str += instance['State']['Name'] + ' '
+            str += instance['KeyName'] + ' '
+            str += instance['Placement']['AvailabilityZone'] + ' '
+            str += instance['PublicDnsName'] + ' '
+
+            if (name and (name == instancename)):
+                if (not instance['InstanceId'] in ids):
+                    if (not filter or (filter in str)):
+                        list.append(instance)
+                        ids.append(instance['InstanceId'])
+
+            elif (id and (id == instance['InstanceId'])):
+                if (not instance['InstanceId'] in ids):
+                    if (not filter or (filter in str)):
+                        list.append(instance)
+                        ids.append(instance['InstanceId'])
+            else:
+                if (filter):
+                    if ((filter in str) and (not instance['InstanceId'] in ids)):
+                        list.append(instance)
+                        ids.append(instance['InstanceId'])
+                elif (not name and not id):
+                    list.append(instance)
+                    ids.append(instance['InstanceId'])
     return list
 
 
-def ec2_create(name="", verbose=False):
+def ec2_create(name="", verbose=False, keyname=""):
     """ Create an EC2 instance """
+
+    amazon_linux_2="ami-0cc814d99c59f3789"
+    jenkins_7="ami-0e019d7e4645e931d"
+    ubuntu="ami-0afd55c0c8a52973a"
+    ami=ubuntu
+
+    instancetype='t2.micro'
+    sg='sg-0b864f5c5c2ceb714'
+
     if verbose:
-        print ('creating key pair name=' + name)
+        print ('creating an EC2 instance')
 
     if (not name):
         basename = os.environ.get('BASENAME')
-        name = basename + '_' + 'ec2-keypair'
-    filename = name + '.pem'
+        name = basename + '_ec2-i' if (basename) else 'ec2-i'
 
-    # create a file to store the key locally
-    outfile = open(filename,'w')
+    # create a new EC2 instance
+    instances = ec2.create_instances(
+        ImageId=ami,
+        MinCount=1,
+        MaxCount=1,
+        InstanceType=instancetype,
+        KeyName=keyname,
+        SecurityGroupIds=[sg],
+        TagSpecifications=[{
+            'ResourceType': 'instance',
+            'Tags': [
+                {
+                    'Key': 'Name',
+                    'Value': name
+                },
+            ]
+        }]
+    )
 
-    # call the boto ec2 function to create a key pair
-    key_pair = ec2.create_key_pair(KeyName=name)
-
-    # capture the key and store it in a file
-    KeyPairOut = str(key_pair.key_material)
-    if verbose:
-        print(KeyPairOut)
-    outfile.write(KeyPairOut)   
-
-
-def ec2_delete(ids = [], verbose=False):
-    """ Delete a list of EC2 instances from thier ids """
-    for id in ids:
+def ec2_delete(list = [], verbose=False):
+    """ Delete a list of EC2 instances """
+    for instance in list:
+        id=instance['InstanceId']
         if (verbose):
-            print ('deleting key pair id=' + id['KeyPairId'])
-        response = ec2_client.delete_key_pair(KeyPairId=id['KeyPairId'])
-        filename = id['KeyName'] + '.pem'
-        os.remove(filename) 
+            print ('deleting EC2 id=' + id)
+        ec2.instances.filter(InstanceIds = [id]).terminate()
+
+def ec2_print_list(list, verbose=False):
+    """ Print a list of EC2 instances """
+    for instance in list:
+        name = ""
+        if 'Tags' in instance:
+            for tag in instance['Tags']:
+                if (tag['Key'] == 'Name'):
+                        name = tag['Value']
+        
+        print(name, instance['InstanceId'], instance['InstanceType'],
+              instance['KeyName'], instance['State']['Name'],
+              instance['Placement']['AvailabilityZone'],
+              instance['PublicDnsName']
+        )
+
+def ec2_stop(list, verbose=False):
+    """ Stop EC2 instances """
+    for instance in list:
+        id=instance['InstanceId']
+        if (verbose):
+            print ('stopping EC2 id=' + id)
+        ec2_client.stop_instances(InstanceIds=[id])
+
+def ec2_resume(list, verbose=False):
+    """ Resume EC2 instances """
+    for instance in list:
+        id=instance['InstanceId']
+        if (verbose):
+            print ('resuming EC2 id=' + id)
+        ec2_client.start_instances(InstanceIds=[id])
