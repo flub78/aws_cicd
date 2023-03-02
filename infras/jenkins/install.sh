fe0026027aa1b8cb034a8b8dc1e86ad4f7a77d6e
Flub78 <frederic.peignot@free.fr>
2023-03-02 12:00:46 +0100

    a job to install ansible on the jenkins server

--- infras/jenkins/install.sh
+++ infras/jenkins/install.sh
@@ -14,6 +14,8 @@ ansible-playbook --inventory hosts --key-file $TF_VAR_PRIVATE_KEY $PLAYBOOK/lamp
 
 ansible-playbook --inventory hosts --key-file $TF_VAR_PRIVATE_KEY $PLAYBOOK/jenkins.yml
 
+ansible-playbook --inventory hosts --key-file $TF_VAR_PRIVATE_KEY $PLAYBOOK/jenkins_jobs_multi.yml
+
 # Jenkins configuration
 
 Until now there is still a manual step in the jenkins configuration. Once jenkins is installed the user must go to the web interface and provide tha password to unlock jenkins.
@@ -26,9 +28,26 @@ TODO: automate the creation of token for jenkins-cli
 cd ~/jenkins-cli
 java -jar jenkins-cli.jar -s http://ratus.flub78.net:8080/ -auth @jenkins_token who-am-i
 
+# jenkins shortcut
+# export JENKINS_USER_ID=jenkins
+# export JENKINS_API_TOKEN=xxxxxxxxxxxxxxxxxxxxxxxxx
+# export JENKINS_URL=http://ratus.flub78.net:8080
+# alias jenkins='java -jar jenkins-cli.jar'
+
+# $ jenkins who-am-i
+# Authenticated as: jenkins
+# Authorities:
+#   authenticated
+
 java -jar jenkins-cli.jar -s http://ratus.flub78.net:8080/ -auth @jenkins_token install-plugin phing
 java -jar jenkins-cli.jar -s http://ratus.flub78.net:8080/ -auth @jenkins_token install-plugin htmlpublisher
-		
+java -jar jenkins-cli.jar -s http://ratus.flub78.net:8080/ -auth @jenkins_token install-plugin plot
+java -jar jenkins-cli.jar -s http://ratus.flub78.net:8080/ -auth @jenkins_token install-plugin jdepend
+java -jar jenkins-cli.jar -s http://ratus.flub78.net:8080/ -auth @jenkins_token install-plugin subversion
+java -jar jenkins-cli.jar -s http://ratus.flub78.net:8080/ -auth @jenkins_token install-plugin violations
+java -jar jenkins-cli.jar -s http://ratus.flub78.net:8080/ -auth @jenkins_token install-plugin warnings-ng
 java -jar jenkins-cli.jar -s http://ratus.flub78.net:8080/ -auth @jenkins_token safe-restart
 
 ansible-playbook --inventory hosts --key-file $TF_VAR_PRIVATE_KEY $PLAYBOOK/jenkins_jobs_multi.yml
+
+
